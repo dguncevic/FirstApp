d9eb484f0fa8050ad2dcc08eedab2d15aeb81d4a
Domagoj <132295836+dguncevic@users.noreply.github.com>
2024-06-05 10:05:24 +0200

    work

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,18 +1,75 @@
-
 import './App.css';
-import InputField from './components/InputField.jsx';
+import { useState } from 'react';
+import { Button, Input, DatePicker, Table } from 'antd';
+import { DATA_SOURCE } from './DataSource';
+import { COLUMNS } from './Columns';
 
 function App() {
 
-  return(
-    <>
-      <InputField inputType="text" placeHolder="Ime"></InputField>
-      <InputField inputType="text" placeHolder="Prezime"></InputField>
-      <InputField inputType="number" placeHolder="Godine"></InputField>
-      <button>OK</button>
-      <button>Clear</button>
-   
-  </>
+  const [inputArr, setInputArr] = useState([]);
+
+  const [inputData, SetInputData] = useState({
+    name: "",
+    lastname: "",
+    dob: "",
+    adress: ""
+
+  })
+
+
+  function changeHandle(e) {
+
+    SetInputData({ ...inputData, [e.target.name]: e.target.value })
+  }
+
+  let { name, lastname, dob, adress } = inputData;
+
+  function changehandle() {
+    setInputArr([...inputArr, { name, lastname, dob, adress }]);
+    console.log("^saved array so far")
+    console.log(inputData);
+    console.log("^data you put in")
+    SetInputData({ name: "", lastname: "", dob: "", adress: "" });
+  }
+
+  const handleLogInputValue = (event) => {
+    console.log(event.target.value);
+  }
+
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
+  const handleOkClick = () => {
+    console.log();
+  }
+  const handleNameChange = (target) => {
+    console.log(target.value);
+  }
+  const handleLastnameChange = (target) => {
+    console.log(target.value);
+  }
+  const handleAdressChange = (target) => {
+    console.log(target.value);
+  }
+
+
+  return (
+    <div gap="small">
+      <Input placeholder="Ime" variant="filled" onChange={handleNameChange}></Input>
+      <Input placeholder="Prezime" onChange={handleLastnameChange}></Input>
+      <Input placeholder="Adresa" onChange={handleAdressChange}></Input>
+      <DatePicker onChange={onChange} />
+      <div>
+        <Button type="primary" onClick={handleLogInputValue}>OK</Button>
+        <Button>Clear</Button></div>
+
+      <>
+        <Table dataSource={DATA_SOURCE} columns={COLUMNS}></Table>
+      </>
+    </div>
+
+
   );
 }
 
