61d373eb5660f048e9ff119fce6f99ec156bf2e3
Domagoj <132295836+dguncevic@users.noreply.github.com>
2024-06-05 20:42:47 +0200

    work work work

---
+++ homework/src/Columns.jsx
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title:'Lastname',
+      dataIndex: 'lastname',
+      key: 'lastname'
+    },
+    {
+      title: 'DOB',
+      dataIndex: 'dob',
+      key: 'dob',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
---
+++ homework/src/DataSource.jsx
@@ -0,0 +1,16 @@
+export const DATA_SOURCE = [
+    {
+      key: '1',
+      name: 'Domagoj',
+      lastname: 'Beslik',
+      dob: '1993-03-20',
+      address: 'Dyakovstine 10',
+    },
+    {
+      key: '2',
+      name: 'Domagoj',
+      lastname: 'Gunkevik',
+      dob: '1997-12-20',
+      address: 'Sarena 4',
+    },
+  ];
